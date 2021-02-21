a2e6e7ddd9cd18974f7bcf4407ab7055347e12e8
sigdelbjay <sigdelbjay@gmail.com>
2021-02-21 11:04:58 +1100

    Setup project

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <h3>Hello World!</h3>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
